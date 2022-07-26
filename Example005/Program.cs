47531a94613630748f148015a7102e7d26d73d16
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-26 21:21:02 +0300

    Add Example 005 Task about the remainder

---
+++ Example005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное 3-значное число: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            if (Num>99 && Num<1000)
+            {
+                int z = Num % 10;
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Введенное число не соответствует условию задачи.");
+            }
+        }
+    }
+}
