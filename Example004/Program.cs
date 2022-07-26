c8f65f878e02d6f434f63e59de2ba4ac9bdd1d6b
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-26 20:48:42 +0300

    Add Example 004 The set of integers

---
+++ Example004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int x = (-N);
+
+            while(x<(N+1) && x> -(N+1))
+            {
+                Console.Write(x);
+                Console.Write(" ,");
+                x++;
+
+            }
+
+        }
+    }
+}
