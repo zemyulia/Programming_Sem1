8c63a7c55f39d33789d003cccf0d2633af9a95d9
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 14:23:08 +0300

    Add Homework. Example 8, Seminar 1.

---
+++ Homework/Ex8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            int count = 1;
+
+            while (count<=Number)
+            {
+                if(count%2==0)
+                {
+                    Console.WriteLine(count);
+                    count++;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+        }
+    }
+}
