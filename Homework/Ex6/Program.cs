b00a93a26ac2eab7f3e720c308651f933d696d77
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 14:04:10 +0300

    Add Homework. Example 6, Seminar 1.

---
+++ Homework/Ex6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            if(Number%2==0)
+            {
+                Console.WriteLine("Введенное число - четное.");
+            }
+            else
+            {
+                Console.WriteLine("Введенное число - нечетное.");
+            }
+        }
+    }
+}
