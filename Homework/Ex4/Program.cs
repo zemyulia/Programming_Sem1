03eaa0c484a209a0f1be12f4223230a43c2dfd85
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 13:56:41 +0300

    Add Homework. Example 4, Seminar 1.

---
+++ Homework/Ex4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int NumA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int NumB = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число: ");
+            int NumС = Convert.ToInt32(Console.ReadLine());
+
+            int max = 0;
+
+            if(NumA==NumB && NumB==NumС)
+            {
+                Console.WriteLine("Числа равны");
+            }
+            else
+            {
+               if(NumA>NumB && NumA>NumС)
+                {
+                    max = NumA;
+                    Console.Write("Max = ");
+                    Console.WriteLine(max);
+                }
+               
+               else 
+                    {
+                        if(NumB>NumС && NumB>NumA)
+                            {
+                                max = NumB;
+                                Console.Write("Max = ");
+                                Console.WriteLine(max);
+                            }
+                        else 
+                            {
+                                if (NumС>NumA && NumС>NumB)
+                                    {
+                                        max = NumС;
+                                        Console.Write("Max = ");
+                                        Console.WriteLine(max);
+                                    }
+                            }
+                    }
+
+            }
+        }
+    }
+}
+
+
