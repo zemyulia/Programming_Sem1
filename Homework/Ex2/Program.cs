ed5a07776884ab9660211e946b1164815b4e1be5
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 12:44:46 +0300

    Add Homework. Example2, Seminar 1.

---
+++ Homework/Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int NumA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число: ");
+            int NumB = Convert.ToInt32(Console.ReadLine());
+
+            if(NumA==NumB)
+            {
+                Console.WriteLine("Числа равны");
+            }
+            else
+            {
+                if(NumA>NumB)
+                {
+                    Console.Write("Max = ");
+                    Console.WriteLine(NumA);
+                }
+                else
+                {
+                    Console.Write("Max = ");
+                    Console.WriteLine(NumB);
+                }
+            }
+        }
+    }
+}
